3117fef8ac9f1d66143be5d0d270d218573554c7
Ian Rambo <ian@Ians-MacBook-Pro.local>
2020-06-04 01:36:40 -0500

    add README; process substitution with grep works for GNU and non-GNU grep

--- extract_depthfile.sh
+++ extract_depthfile.sh
@@ -3,7 +3,6 @@
 #Maintainer: Ian Rambo :: ian.rambo@utexas.edu
 #Thirteen... that's a mighty unlucky number... for somebody!
 
-
 usage="$(basename "$0"): extract coverage profiles for a genome.
 
 If extracting for mmgenome2, make sure --out ends with _cov
@@ -12,39 +11,46 @@ extract_depthfile.sh -f <FASTA> -d <DEPTHFILE> -v <1|0> > out_cov
 
 where:
 
-   -h | --help  --- show this help message
-   -f | --fasta --- FASTA file to extract depths for
-   -d | --depth --- depth file from jgi_summarize_bam_contig_depths
-   -o | --out   --- output depthfile subset
-   -v | --novar --- exclude depthfile variance columns (0|1). Default=1
+   -h --- show this help message
+   -f --- FASTA file to extract depths for
+   -d --- depth file from jgi_summarize_bam_contig_depths
+   -o --- output depthfile subset
+   -v --- exclude depthfile variance columns (0|1). Default=1
 
     "
 
 novar=1
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -f | --fasta )          shift
-                                fasta=$1
-                                ;;
-        -d | --depth )          shift
-                                depth=$1
-                                ;;
-        -o | --out )            shift
-                                outdepth=$1
-                                ;;
-        -v | --var )            shift
-                                novar=$1
-                                ;;
-        -h | --help )           echo "$usage"
-                                exit
-                                ;;
-        * )                     echo $usage
-                                exit 1
+while getopts ':hf:d:o:v:' option; do
+    case "${option}" in
+    h) echo "$usage"
+       exit ;;
+    f) fasta=${OPTARG};;
+    d) depth=${OPTARG};;
+    o) outdepth=${OPTARG};;
+    v) novar=${OPTARG};;
+
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1 ;;
+    \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+        echo "$usage" >&2
+        exit 1 ;;
     esac
-    shift
 done
+
+shift $((OPTIND - 1))
 #=============================================================================
+function check_file() {
+    if [ ! -f "$1" ]; then
+        echo "could not find $1, no such file"
+        exit 1
+    else
+        return 0
+    fi
+}
+#-----------------------------------------------------------------------------
+check_file $fasta && check_file $depth
 
 depthfile_ncol=$(awk -F'\t' '{print NF; exit}' ${depth})
 
@@ -57,10 +63,10 @@ else
     exit 1
 fi
 
-#Write the output depthfile header
+#Write the header line (contig ID and sample depths)
 head -n 1 $depth | cut -f1,${depth_colseq} -d $'\t' > $outdepth
-#Write the depthfile columns for the contigs
-grep '>' $fasta | sed -e "s/^>//g" | \
-awk '{gsub("$","[[:space:]]*",$0); print;}' | \
-grep -f - $depth | \
+
+#Write the depthfile rows for the contigs
+grep -f <(grep '>' $fasta | sed -e "s/^>//g" | \
+awk '{gsub("$","[[:space:]]*",$0); print;}') $depth | \
 cut -f1,${depth_colseq} -d $'\t' >> $outdepth
